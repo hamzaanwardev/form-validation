9c3ebcb2dffa4326347fe7e64bf9111acaec7915
Hamza Anwar <88555920+hamzaanwardev@users.noreply.github.com>
2021-09-25 03:59:33 +0500

    Completed

--- script.js
+++ script.js
@@ -11,9 +11,13 @@ function showError(input, message) {
     small.innerText = message;
 }
 
-    function isValidEmail(email) {
+    function isValidEmail(input) {
         const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-            return re.test(String(email).toLowerCase());
+           if (re.test(input.value.trim())) {
+            showSuccess (input);
+           }else {
+               showError (input, 'Email IS Invalid');
+           }
     }
 
     
@@ -38,6 +42,13 @@ function showError(input, message) {
         }
     }
 
+    function checkPasswordsMatch (input1, input2) {
+        if(input1.value !== input2.value) {
+            showError (input2, 'Password do not match');
+
+        }
+    }
+
     function getFieldName(input) {
         return input.id.charAt(0).toUpperCase() + input.id.slice(1);
     }
@@ -54,5 +65,7 @@ form.addEventListener('submit', function(e) {
     e.preventDefault();
     checkRequired([username, email, password, password2]);
     checkLength(username, 3, 15);
-    checkLength(password, 6, 25)
+    checkLength(password, 6, 25);
+    checkEmail(email);
+    checkPasswordsMatch(input1, input2);
 });
