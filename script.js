6a7619477760eba70ba380182fa4ba9528cdc828
Hamza Anwar <88555920+hamzaanwardev@users.noreply.github.com>
2021-09-24 23:50:31 +0500

    updated JavaScript

--- script.js
+++ script.js
@@ -1,24 +1,32 @@
 const form = document.getElementById('form');
-const userName = document.getElementById('username');
+const username = document.getElementById('username');
 const email = document.getElementById('email');
 const password = document.getElementById('password');
 const password2 = document.getElementById('password2');
 
-function showError (input, message) {
+function showError(input, message) {
     const formControl = input.parentElement;
     formControl.className = 'form-control error';
-    const small = document.querySelector('small');
+    const small = formControl.querySelector('small');
     small.innerText = message;
 }
 
-    function isValidEmail () {
+    function isValidEmail(email) {
         const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
             return re.test(String(email).toLowerCase());
     }
 
+    
+
+    function checkRequiredFields(input) {
+        inputArr.forEach(function (input) {
+                console.log(input.value);
+        });
+    }
 
 
-function showSuccess (input) {
+
+function showSuccess(input) {
     const formControl = input.parentElement;
     formControl.className = 'form-control success';
 }
@@ -26,19 +34,24 @@ function showSuccess (input) {
 
 form.addEventListener('submit', function(e) {
     e.preventDefault();
-    if(userName.value === '' ) {
-        showError (userName, 'Username is required');
+    if(username.value === '' ) {
+        showError(username, 'Username is required');
     } else {
-        showSuccess (userName);
+        showSuccess(username);
     }
+    console.log(username.value)
+
+    console.log(email.value);
+    console.log(password.value);
+    console.log(password2.value);
 
     if (email.value === '' ) {
-        showError (email, 'Email is Required');
+        showError(email, 'Email is Required');
     } else if (!isValidEmail(email.value)) {
-        showError (email, 'Email is not Valid'); 
+        showError(email, 'Email is not Valid'); 
     } 
     else {
-        showSuccess (email);
+        showSuccess(email);
     }
 
     if (password.value === '' ) {
@@ -52,7 +65,4 @@ form.addEventListener('submit', function(e) {
     } else {
         showSuccess (password2);
     }
-
-
-    console.log('form Submitted successfully');
 });
